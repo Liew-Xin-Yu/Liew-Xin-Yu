c331ddfe982780b9a438437719776bbead3e69d8
JADE-LIEW <96647729+Liew-Xin-Yu@users.noreply.github.com>
2022-03-18 23:10:19 +0800

    Create hello.cpp

---
+++ hello.cpp
@@ -0,0 +1,9 @@
+Hi, I'm JADE, currently a beginner in c++.
+Vigorously learning c++ with passion, good luck to me!
+
+Current disadvantage in c++:
+-Loops (nested for loop etc)
+-Functioning loops (making menus, small programs)
+
+Current advantage in c++:
+-Not stopping when facing challenges!
